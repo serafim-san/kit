26c8b5530855536551f2febd5f1f430edc3dca6c
Serafim Gavrilov <serafim.g@santiment.net>
2026-04-08 20:14:56 -0300

    fix: use dynamic import for WebkitSvg in storybook main.ts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,6 @@ import type { StorybookConfig } from '@storybook/sveltekit'
 
 import { mergeConfig } from 'vite'
 
-import { WebkitSvg } from '../plugins/vite.js'
 
 const config: StorybookConfig = {
   stories: ['../src/stories/**/*.mdx', '../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -23,6 +22,7 @@ const config: StorybookConfig = {
   },
 
   async viteFinal(config) {
+    const { WebkitSvg } = await import('../plugins/vite.js')
     const result = mergeConfig(config, {
       plugins: [WebkitSvg()],
       sever: {
